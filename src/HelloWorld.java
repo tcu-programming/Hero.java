5f0a2c937b3f49e1ca24d46ef35473aa7ac0be72
curry303030 <1292515867@qq.com>
2019-02-11 12:23:15 +0800

    HelloWorld.java

---
+++ src/HelloWorld.java
@@ -0,0 +1,9 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        short a=1;
+        short b=2;
+        short c=(short)(a+b);
+        int d=a+b;
+        System.out.println(d+""+c);
+    }
+}
