230e2b45f9f08633acdb09de66cd07cf6f99850a
curry303030 <1292515867@qq.com>
2019-02-14 20:56:34 +0800

    HelloWorld3.java

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,32 +1,20 @@
-public class HelloWorld {
-    int i=1;
-    int j=i;//其他属性可以访问i
+public class HelloWorld{
+    public static void main(String[] args) {
+        int a=6;
+        long b=7;
+        int c=(int)(a+b); //因为计算的结果为long，所以需要进行强制转换
+        long d=a+b;
 
-    public void method1() {
-        System.out.println(i);
-    }
-              //方法1里可以访问i
-    public void method2(){
-        System.out.println(i);
-    }
-              //方法2里可以访问i
 
-    public void method3(int a) {
-        System.out.println(a);
-    }         //只能在方法3中引用a
+        byte a1=1;
+        byte a2=2;
+        byte c1=(byte)(a1+a2);
+        int d1=a1+a2;
 
-    public void method4() {
-        System.out.println(a);
-    }         //方法4中不能访问a
-    int m=a;  //类里面也不能访问a
-    public void method5() {
-        int b=5;
-        System.out.println(b);
-        {
-            System.out.println(b); //可以访问b
-            int c=6;
-            System.out.println(c);//可以访问c
-        }
-        System.out.println(c);//不能访问c
+        int i=5;
+        int j=2;
+        System.out.println(i%j);
     }
+
+
 }
