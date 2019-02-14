bea1b6c439997745e7f453d0a338dd91a7e9368b
curry303030 <1292515867@qq.com>
2019-02-14 17:39:20 +0800

    HelloWorld2.java

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,9 +1,32 @@
 public class HelloWorld {
-    public static void main(String[] args) {
-        short a=1;
-        short b=2;
-        short c=(short)(a+b);
-        int d=a+b;
-        System.out.println(d+""+c);
+    int i=1;
+    int j=i;//其他属性可以访问i
+
+    public void method1() {
+        System.out.println(i);
+    }
+              //方法1里可以访问i
+    public void method2(){
+        System.out.println(i);
+    }
+              //方法2里可以访问i
+
+    public void method3(int a) {
+        System.out.println(a);
+    }         //只能在方法3中引用a
+
+    public void method4() {
+        System.out.println(a);
+    }         //方法4中不能访问a
+    int m=a;  //类里面也不能访问a
+    public void method5() {
+        int b=5;
+        System.out.println(b);
+        {
+            System.out.println(b); //可以访问b
+            int c=6;
+            System.out.println(c);//可以访问c
+        }
+        System.out.println(c);//不能访问c
     }
 }
