0f6bd510b9c5fc803bfe219e96879d40bdc3ae5e
curry303030 <1292515867@qq.com>
2019-02-17 21:23:21 +0800

    HelloWorld7(阶乘计算).java

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,28 +1,14 @@
 import java.util.Scanner;
 public class HelloWorld{
     public static void main(String[] args) {
-       Scanner s=new Scanner(System.in);
-        System.out.println("请输入月份");
-       int month=s.nextInt();
-       switch (month){
-           case 1:
-           case 2:
-           case 3:
-               System.out.println("春天");
-               break;
-           case 4:
-           case 5:
-           case 6:
-               System.out.println("夏天");
-               break;
-           case 7:
-           case 8:
-           case 9:
-               System.out.println("秋天");
-               break;
-               default:
-                   System.out.println("这是什么鬼？");
-       }
-
+     Scanner s=new Scanner(System.in);
+     System.out.println("请输入一个整数");
+     int a=s.nextInt();
+     int sum=1;
+     while (a>1){
+         sum=sum*a;
+         a--;
+     }
+        System.out.println("这个数的阶乘是"+sum);
     }
 }
